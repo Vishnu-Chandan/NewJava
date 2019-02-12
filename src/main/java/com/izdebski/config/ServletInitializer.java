2d7fcaa034ebfcf2d95f30e71b97e092ea537f7a
Ivan Izdepskyi <iizdepskyy112@gmail.com>
2019-02-12 11:39:25 +0200

    Spring MVC HelloWorld with Java Based configuration_second Approach

--- src/main/java/com/izdebski/config/ServletInitializer.java
+++ src/main/java/com/izdebski/config/ServletInitializer.java
@@ -1,24 +1,22 @@
 package com.izdebski.config;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration.Dynamic;
+public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-
-public class ServletInitializer implements WebApplicationInitializer {
 
     @Override
-    public void onStartup(ServletContext container) throws ServletException {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.register(WebConfig.class);
-        context.setServletContext(container);
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
 
-        Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(context));
-        servlet.setLoadOnStartup(1);
-        servlet.addMapping("/");
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
     }
 }
